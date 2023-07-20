bd3eb1618c9766ea6c5e7f8f0273aa39ba4cf02a
Soprak26 <135386711+Soprak26@users.noreply.github.com>
2023-07-20 19:26:13 +0800

    Deployed website

--- src/components/realtimeData/index.js
+++ src/components/realtimeData/index.js
@@ -1,122 +1,322 @@
-import React from 'react';
+
 import StartFirebase from '../firebaseConfig/index';
-import { ref, onValue } from 'firebase/database';
+import { ref, onValue, get } from 'firebase/database';
 import { Table } from 'react-bootstrap';
 import { CrudPanel } from './CrudPanel';
+import React from 'react';
+
 
 
 const db = StartFirebase();
 let UniqueNumber = 0;
 
 export class RealtimeDatainDatabase extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            tableData: []
-        }
+  constructor() {
+    super();
+    this.state = {
+      tableData: []
     }
+  }
 
-    componentDidMount() {
-        const dbRef = ref(db, 'Database');
+  componentDidMount() {
+    const dbRef = ref(db, 'Database');
 
-        onValue(dbRef, (snapshot) => {
-            let records = [];
-            snapshot.forEach(childSnapshot => {
-                let keyName = childSnapshot.key;
-                let data = childSnapshot.val();
-                records.push({ "key": keyName, "data": data });
-            });
-            this.setState({ tableData: records });
-        });
-    }
+    onValue(dbRef, (snapshot) => {
+      let records = [];
+      snapshot.forEach(childSnapshot => {
+        let keyName = childSnapshot.key;
+        let data = childSnapshot.val();
+        records.push({ "key": keyName, "data": data });
+      });
+      this.setState({ tableData: records });
+    });
+  }
 
 
-    render() {
-        return (
-            <Table className='container w-75 text-center' bordered striped variant='secondary'>
-                <thead>
-                    <tr>
-                        
-                        <th>#</th>
-                        <th>License Plate</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Type</th>
-                        <th>Control Panel</th>
-                    </tr>
-                </thead>
+  render() {
+    return (
+      <Table className='container w-75 text-center' bordered striped variant='secondary'>
+        <thead>
+          <tr>
 
-                <tbody>
-                    {this.state.tableData.map((row, index) => {
-                        return (
-                            <tr key={UniqueNumber++}>
-                                <td>{index + 1}</td>
-                                <td>{row.key}</td>
-                                <td>{row.data.firstname}</td>
-                                <td>{row.data.lastname}</td>
-                                <td>{row.data.type}</td>
-                                <td><CrudPanel platenumber={row.key} record={row.data} /></td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </Table>
-        )
-    }
+            <th>#</th>
+            <th>License Plate</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Type</th>
+            <th>Control Panel</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.state.tableData.map((row, index) => {
+            return (
+              <tr key={UniqueNumber++}>
+                <td>{index + 1}</td>
+                <td>{row.key}</td>
+                <td>{row.data.firstname}</td>
+                <td>{row.data.lastname}</td>
+                <td>{row.data.type}</td>
+                <td><CrudPanel platenumber={row.key} record={row.data} /></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    )
+  }
 }
 
 export class RealtimeDatainParkedCarLog extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            tableData: []
-        }
+  constructor() {
+    super();
+    this.state = {
+      tableData: []
     }
+  }
 
-    componentDidMount() {
-        const dbRef = ref(db, 'ParkedCarLog');
+  componentDidMount() {
+    const dbRef = ref(db, 'parkedcarlog');
 
-        onValue(dbRef, (snapshot) => {
-            let records = [];
-            snapshot.forEach(childSnapshot => {
-                let keyName = childSnapshot.key;
-                let data = childSnapshot.val();
-                records.push({ "key": keyName, "data": data });
-                
-            });
-            this.setState({ tableData: records });
-        });
-            
-    }
+    onValue(dbRef, (snapshot) => {
+      let records = [];
+      snapshot.forEach(childSnapshot => {
+        let keyName = childSnapshot.key;
+        let data = childSnapshot.val();
+        records.push({ "key": keyName, "data": data });
 
-    render() {
-        return (
-            <Table className='container w-75 text-center' bordered striped variant='secondary'>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>License Plate</th>
-                        <th>Date</th>
-                        <th>Time In</th>
-                        <th>Time Out</th>
-                    </tr>
-                </thead>
+      });
+      this.setState({ tableData: records });
+    });
 
-                <tbody>
-                    {this.state.tableData.map((row, index) => {
-                        return (
-                            <tr>
-                                <td>{index + 1}</td>
-                                <td>{row.key}</td>
-                                <td>{row.data.date}</td>
-                                <td>{row.data.timein}</td>
-                                <td>{row.data.timeout}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </Table>
-        )
-    }
+  }
+
+  render() {
+    return (
+      <Table className='container w-75 text-center' bordered striped variant='secondary'>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>License Plate</th>
+            <th>Date</th>
+            <th>Time In</th>
+            <th>Time Out</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.state.tableData.map((row, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{row.key}</td>
+                <td>{row.data.date}</td>
+                <td>{row.data.timein}</td>
+                <td>{row.data.timeout}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    )
+  }
 }
+
+
+export class VacantCounter1Cars extends React.Component {
+  state = {
+    value: null,
+  };
+
+  componentDidMount() {
+    this.fetchValue();
+    this.setupAutoUpdate();
+  }
+
+  fetchValue = async () => {
+    const dbRef = ref(db, "/vacantcounter1/cars"); // Replace with your desired path
+
+    try {
+      const snapshot = await get(dbRef);
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    } catch (error) {
+      console.error("Error fetching value from Firebase:", error);
+    }
+  };
+  setupAutoUpdate = () => {
+    const valueRef = ref(db, "/vacantcounter1/cars"); // Replace with your desired path
+
+    onValue(valueRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className='border-3 border-red-600 bg-red-400  w-[250px] h-[140px] items-center justify-center flex'>
+        <p className='text-center text-2xl font-bold '>Occupied: {value}</p>
+      </div>
+    );
+  }
+}
+
+export class VacantCounter1Vacant extends React.Component {
+  state = {
+    value: null,
+  };
+
+  componentDidMount() {
+    this.fetchValue();
+    this.setupAutoUpdate();
+  }
+
+  fetchValue = async () => {
+    const dbRef = ref(db, "/vacantcounter1/vacant"); // Replace with your desired path
+
+    try {
+      const snapshot = await get(dbRef);
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    } catch (error) {
+      console.error("Error fetching value from Firebase:", error);
+    }
+  };
+  setupAutoUpdate = () => {
+    const valueRef = ref(db, "/vacantcounter1/vacant"); // Replace with your desired path
+
+    onValue(valueRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className='border-3 border-green-600 bg-green-400 w-[250px] h-[140px] items-center justify-center flex'>
+        <p className='text-center text-2xl font-bold '>Vacant: {value}</p>
+      </div>
+    );
+  }
+}
+
+
+export class VacantCounter2Cars extends React.Component {
+  state = {
+    value: null,
+  };
+
+  componentDidMount() {
+    this.fetchValue();
+    this.setupAutoUpdate();
+  }
+
+  fetchValue = async () => {
+    const dbRef = ref(db, "/vacantcounter2/cars"); // Replace with your desired path
+
+    try {
+      const snapshot = await get(dbRef);
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    } catch (error) {
+      console.error("Error fetching value from Firebase:", error);
+    }
+  };
+  setupAutoUpdate = () => {
+    const valueRef = ref(db, "/vacantcounter2/cars"); // Replace with your desired path
+
+    onValue(valueRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className='border-3 border-red-600 bg-red-400  w-[250px] h-[140px] items-center justify-center flex'>
+        <p className='text-center text-2xl font-bold '>Occupied: {value}</p>
+      </div>
+    );
+  }
+}
+
+export class VacantCounter2Vacant extends React.Component {
+  state = {
+    value: null,
+  };
+
+  componentDidMount() {
+    this.fetchValue();
+    this.setupAutoUpdate();
+  }
+
+  fetchValue = async () => {
+    const dbRef = ref(db, "/vacantcounter2/vacant"); // Replace with your desired path
+
+    try {
+      const snapshot = await get(dbRef);
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    } catch (error) {
+      console.error("Error fetching value from Firebase:", error);
+    }
+  };
+  setupAutoUpdate = () => {
+    const valueRef = ref(db, "/vacantcounter2/vacant"); // Replace with your desired path
+
+    onValue(valueRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const value = snapshot.val();
+        this.setState({ value });
+      }
+    });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className='border-3 border-green-600 bg-green-400 w-[250px] h-[140px] items-center justify-center flex'>
+        <p className='text-center text-2xl font-bold '>Vacant: {value}</p>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/pages_admin/ParkedCarLog.jsx
+++ src/pages_admin/ParkedCarLog.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { RealtimeDatainParkedCarLog } from '../components/realtimeData'
+import { RealtimeDatainParkedCarLog, VacantCounter1Cars, VacantCounter1Vacant, VacantCounter2Cars, VacantCounter2Vacant } from '../components/realtimeData'
 
 const ParkedCarLog = () => {
   return (
     <>
-    <Navbar></Navbar>
-    <div className = 'text-center text-4xl text-red-700 text-bold py-4 font-bold' >Parked Car Log</div>
-    <div><RealtimeDatainParkedCarLog/></div>
+      <Navbar></Navbar>
+      <div className='text-center text-4xl text-red-700 py-4 font-bold' >Parked Car Log</div>
+      <div><RealtimeDatainParkedCarLog /></div>
+      <div className='text-center text-4xl text-red-700 py-4 font-bold'>Camera 1</div>
+      <br />
+      <div className='flex items-center justify-center space-x-4'>
+        <VacantCounter1Cars />
+        <VacantCounter1Vacant />
+      </div>
+      <div className='text-center text-4xl text-red-700 py-4 font-bold'>Camera 2</div>
+      <br />
+      <div className='flex items-center justify-center space-x-4'>
+        <VacantCounter2Cars />
+        <VacantCounter2Vacant />
+      </div>
     </>
   )
 }
